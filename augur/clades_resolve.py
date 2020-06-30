caf607e52ebd744529af0427a51cc470cc98165f
Vang Le-Quy <vle@its.aau.dk>
2020-06-30 13:06:56 +0200

    Some big changes
    - Make clade_resolve resolve subclade automatically  based on nextstrain
      mutations and base clades. This includes rewriting clade resolving part.
    - Enable writing output for clade assignment

--- augur/clades_resolve.py
+++ augur/clades_resolve.py
@@ -7,12 +7,25 @@ from Bio import Phylo
 import pandas as pd
 import numpy as np
 from collections import defaultdict
+
+from augur.translate import construct_mut
 from augur.utils import get_parent_name_by_child_name_for_tree, read_node_data, write_json, get_json_name
 from augur.clades import is_node_in_clade, read_in_clade_definitions, get_reference_sequence_from_root_node
 import argparse
+from pathlib import Path
 
 
-def resolve_clades(clade_designations, all_muts, tree, ref=None, support=10, diff=1, depth = 5):
+def get_naive_direct_mutations(tree, all_muts, ref):
+    direct_mutations = {}
+    for node in tree.get_terminals():
+        node_muts = {construct_mut(a, int(pos + 1), d) for pos, (a, d) in
+                     enumerate(zip(ref['nuc'], all_muts[node.name]['sequence']))
+                     if a != d and a != 'N' and d != 'N'}
+        direct_mutations[node.name] = node_muts
+    return direct_mutations
+
+
+def resolve_clades(clade_designations, all_muts, tree, ref=None, support=10, diff=1, depth=3):
     '''
 
     Ensures all nodes have an entry (or auspice doesn't display nicely), tests each node
@@ -35,11 +48,11 @@ def resolve_clades(clade_designations, all_muts, tree, ref=None, support=10, dif
     -------
     dict
         mapping of node to clades
-    TODO: Add option to incorporate manual SNP table from extract_SNPs.py
     '''
 
     clade_membership = {}
-    parents = get_parent_name_by_child_name_for_tree(tree)
+    direct_mutations = {}
+    # parents = get_parent_name_by_child_name_for_tree(tree)
 
     # first pass to set all nodes to unassigned as precaution to ensure attribute is set
     for node in tree.find_clades(order='preorder'):
@@ -52,6 +65,9 @@ def resolve_clades(clade_designations, all_muts, tree, ref=None, support=10, dif
     for node in tree.get_nonterminals():
         for c in node:
             c.up = node
+            c.up.clade = ""
+            c.up.level = 0
+            c.up.accumulated_muts = set()
     tree.root.up = None
     tree.root.sequences = {'nuc': {}}
     if 'aa_muts' in all_muts[tree.root.name]:
@@ -59,53 +75,30 @@ def resolve_clades(clade_designations, all_muts, tree, ref=None, support=10, dif
 
     # attach sequences to all nodes
     for node in tree.find_clades(order='preorder'):
+        node_muts = {construct_mut(a, int(pos+1), d) for pos, (a,d) in
+                            enumerate(zip(ref['nuc'], all_muts[node.name]['sequence']))
+                     if a!=d and a != 'N' and d != 'N' }
+        direct_mutations[node.name] = node_muts
         if node.up:
             node.sequences = {gene: muts.copy() for gene, muts in node.up.sequences.items()}
+            node.accumulated_muts = node.up.accumulated_muts.union(all_muts[node.name]['muts'])
+            acc_muts = node.accumulated_muts
+            if not acc_muts.issubset(node_muts) and node.is_terminal():
+               print(f'Timetree accumulated mutations do not match direct mutations for {node.name}:\n'
+                     f'Unique for timetree: {acc_muts.difference(node_muts)}\n'
+                     f'Intersection: {acc_muts.intersection(node_muts)}\n'
+                     f'Unique for direct: {node_muts.difference(acc_muts)}\n')
+               nn = node.name
+               if nn == "Hungary/SRC-00817/2020":
+                    p = [n.name for n in tree.get_path(node)]
+                    tmp = {n: all_muts[n]['muts'] for n in p }
+                    print(f"Pause debug: {','.join(p)}\n")
         for mut in all_muts[node.name]['muts']:
             a, pos, d = mut[0], int(mut[1:-1]) - 1, mut[-1]
             node.sequences['nuc'][pos] = d
-        if 'aa_muts' in all_muts[node.name]:
-            for gene in all_muts[node.name]['aa_muts']:
-                for mut in all_muts[node.name]['aa_muts'][gene]:
-                    a, pos, d = mut[0], int(mut[1:-1]) - 1, mut[-1]
-
-                    if gene not in node.sequences:
-                        node.sequences[gene] = {}
-                    node.sequences[gene][pos] = d
 
     updated_nodes = []
     new_clades = {}
-    def _resolve_child_clade(clade_name, clade_alleles, subtree, clade_level, depth = depth):
-        if clade_level <= depth:
-            for node in subtree:
-                # if is_node_in_clade(clade_alleles, node, ref):
-                muts = all_muts[node.name]['muts']
-                mut_str = ','.join(muts)
-                cl_name = clade_name
-                if len(muts) >= diff and node.leaf_count >= support:
-                    alleles = []
-                    for mut in all_muts[node.name]['muts']:
-                        a, pos, d = mut[0], int(mut[1:-1]) - 1, mut[-1]
-                        allele = ('nuc', pos, d)
-                        alleles.append(allele)
-                    if node.name not in updated_nodes and\
-                            is_node_in_clade(alleles, node, ref) and\
-                            node not in basal_nodes:
-                        cl_name = cl_name + '/' + mut_str
-                        ## if clade_level > 0:
-                        print(f"New clade: {cl_name}, level: {clade_level}, support: {node.leaf_count}")
-                        clade_membership[node.name] = {'clade_annotation': cl_name, 'clade_membership': cl_name}
-                        new_clades[cl_name] = alleles
-                        updated_nodes.append(node.name)
-
-
-                    clade_level += 1
-                    _resolve_child_clade(cl_name, alleles, node, clade_level)
-                else:
-                    _resolve_child_clade(clade_name, clade_alleles, node, clade_level)
-        else:
-            return
-
     # second pass to assign 'clade_annotation' to basal nodes within each clade
     # if multiple nodes match, assign annotation to largest
     # otherwise occasional unwanted cousin nodes get assigned the annotation
@@ -123,42 +116,62 @@ def resolve_clades(clade_designations, all_muts, tree, ref=None, support=10, dif
             basal_nodes_clades[target_node.name] = clade_name
             basal_nodes.append(target_node)
             clade_membership[target_node.name] = {'clade_annotation': clade_name, 'clade_membership': clade_name}
+            target_node.clade = clade_name
+            target_node.level = 1
 
     # Third pass, resolve names for children clades
-    for node in basal_nodes:
-        clade_name = basal_nodes_clades[node.name]
-        # print(f'Node: {node.name}, cn: {clade_name}')
-        if clade_name in clade_designations:
-            clade_alleles = clade_designations[clade_name]
-            _resolve_child_clade(clade_name, clade_alleles, node, clade_level=0)
-        else:
-            print(f'Unexpected clade name: {clade_name}')
+    for subtree in basal_nodes:
+        for node in subtree.find_clades(order="preorder"):
+            if node.name == tree.root.name and node not in basal_nodes:
+                node.clade = ""
+            muts = all_muts[node.name]["muts"]
+            if node not in basal_nodes:
+                if node.leaf_count >= support and node.name != tree.root.name and 0 < len(muts) < 100 \
+                        and node.up.level < depth:
+                    node.clade = node.up.clade + "/" + ",".join(muts)
+                    node.level = node.up.level + 1
+                    new_clades[node.clade] = muts
+                elif node.up.clade:
+                    node.clade = node.up.clade
+                    node.level = node.up.level
+                else:
+                    print(f"Unexpected node: {node.name}")
+                    node.clade = ",".join(muts)
+            clade_membership[node.name] = {'clade_membership': node.clade}
+            updated_nodes.append(node.name)
+            if len(muts) >= 100:
+                print(f'Excluded outliner node from clade naming: {node.name}, number of muts: {len(muts)}')
+            # if node.up and node.up.level >= depth:
+            #     print(f'Level execeed, node: {node.up.name}, level: {node.up.level}')
 
     # Fourth pass to propagate 'clade_membership'
     # don't propagate if encountering 'clade_annotation'
     for node in tree.find_clades(order='preorder'):
         for child in node:
-            if child.name in clade_membership and 'clade_annotation' not in clade_membership[child.name]:
+            if child.name in clade_membership and 'clade_annotation' not in clade_membership[child.name] and \
+                    node.name not in updated_nodes:
                 clade_membership[child.name]['clade_membership'] = clade_membership[node.name]['clade_membership']
 
-    return clade_membership, new_clades
+    return clade_membership, new_clades, direct_mutations
 
 
 def register_arguments(parser):
-    parser.add_argument('--tree', help="prebuilt Newick -- no tree will be built if provided")
-    parser.add_argument('--mutations', nargs='+',
+    parser.add_argument('-t', '--tree', help="prebuilt Newick -- no tree will be built if provided")
+    parser.add_argument('-m', '--mutations', nargs='+',
                         help='JSON(s) containing ancestral and tip nucleotide and/or amino-acid mutations ')
-    parser.add_argument('--reference', nargs='+',
+    parser.add_argument('-r', '--reference', nargs='+',
                         help='fasta files containing reference and tip nucleotide and/or amino-acid sequences ')
-    parser.add_argument('--clades', type=str, help='TSV file containing clade definitions')
-    parser.add_argument('--new-clades', type=str, help='Write out TSV file containing new clades')
-    parser.add_argument('--max-depth', type=int, default= 5,
-                        help='Maximum depth to search for sub-clades from base clade (default: 5)')
+    parser.add_argument('-c', '--clades', type=str, help='TSV file containing clade definitions')
+    parser.add_argument('-n', '--new-clades', type=str, help='Write out TSV file containing new clades')
+    parser.add_argument('--clade-assignment', '--ca', type=str, help='Write out clade assignment')
+    parser.add_argument('--max-depth', type=int, default=3,
+                        help='Maximum depth to search for sub-clades from base clade')
     parser.add_argument('--min-support', type=int, default=10,
-                        help='Mininum number of sequences to form a new sub-clade (default: 10)')
-    parser.add_argument('--min-mutation', type=int, default= 1,
-                        help='Minimum number of mutations compare to the most recent ancestral sequence to form a new clade, (default: 1)')
-    parser.add_argument('--output-node-data', type=str, help='name of JSON file to save clade assignments to')
+                        help='Mininum number of sequences to form a new sub-clade')
+    parser.add_argument('--min-mutation', type=int, default=1,
+                        help='Minimum number of mutations compare to the most recent ancestral sequence to form a new clade. Defunct, not using')
+    parser.add_argument('--output-node-data', '--ond', type=str, help='name of JSON file to save clade assignments to')
+
 
 def run(args):
     ## read tree and data, if reading data fails, return with error code
@@ -169,32 +182,48 @@ def run(args):
         return 1
     all_muts = node_data['nodes']
 
-    if args.reference:
-        # PLACE HOLDER FOR vcf WORKFLOW.
-        # Works without a reference for now but can be added if clade defs contain positions
-        # that are monomorphic across reference and sequence sample.
-        ref = None
-    else:
-        # extract reference sequences from the root node entry in the mutation json
-        # if this doesn't exist, it will complain but not error.
-        ref = get_reference_sequence_from_root_node(all_muts, tree.root.name)
+    # extract reference sequences from the root node entry in the mutation json
+    # if this doesn't exist, it will complain but not error.
+    ref = get_reference_sequence_from_root_node(all_muts, tree.root.name)
 
     min_support = args.min_support
     min_mutation = args.min_mutation
     clade_designations = read_in_clade_definitions(args.clades)
 
-    clade_membership, new_clades = resolve_clades(clade_designations, all_muts, tree, ref,
-                                                  support=min_support, diff= min_mutation, depth = args.max_depth)
+    clade_membership, new_clades, direct_mutations = resolve_clades(clade_designations, all_muts, tree, ref,
+                                                  support=min_support, diff=min_mutation, depth=args.max_depth)
     if args.new_clades:
         clade_out = open(args.new_clades, 'w')
-        for clade, alleles in new_clades.items():
-            for allele in alleles:
-                clade_out.write(f'{clade}\t{allele[0]}\t{allele[1]+1}\t{allele[2]}\n')
+        for clade, muts in new_clades.items():
+            for mut in muts:
+                clade_out.write(f'{clade}\tnuc\t{mut[1:-1]}\t{mut[-1]}\n')
         print(f"Wrote new clades to: {args.new_clades}")
         clade_out.close()
     out_name = get_json_name(args)
     write_json({'nodes': clade_membership}, out_name)
-    print("clades written to", out_name, file=sys.stdout)
+    print("clades written to", out_name, file=sys.stderr)
+    clade_assignment = args.clade_assignment if args.clade_assignment else Path(out_name).parent.joinpath(
+        "clade_assignment.tsv")
+    write_tip_only = True
+    with open(clade_assignment, "w") as cah:
+        cah.write("strain\tclade\tmutations\tdirect_mutations\n")
+        if write_tip_only:
+            for node in tree.get_terminals():
+                if node.name in clade_membership:
+                    node_path = tree.get_path(node)
+                    muts = []
+                    dmuts = sorted(list(direct_mutations[node.name]), key=lambda x: int(x[1:-1]))
+                    for n in node_path:
+                        muts += all_muts[n.name]['muts']
+
+                    muts = sorted(muts, key=lambda x: int(x[1:-1]))
+                    cah.write(f"{node.name}\t{clade_membership[node.name]['clade_membership']}\t{','.join(muts)}\t{','.join(dmuts)}\n")
+        else:
+            for node in tree.find_clades(order='preorder'):
+                if node.name in clade_membership:
+                    cah.write(
+                        f"{node.name}\t{clade_membership[node.name]['clade_membership']}\t{','.join(all_muts[node.name]['muts'])}\n")
+    print("clade assignments written to", clade_assignment, file=sys.stderr)
 
 
 if __name__ == "__main__":
